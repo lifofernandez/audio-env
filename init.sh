622f40f2abf0de2232777cc7f8ee224df04346bd
lifofernandez <lifofernandez@gmail.com>
2018-09-28 12:02:58 -0300

    agrego ruuteo canales midi

--- init.sh
+++ init.sh
@@ -22,8 +22,8 @@ jack_lsp &&
 jack_connect fluidsynth:left system:playback_1 
 jack_connect fluidsynth:right system:playback_2 
 
+qmidiroute ruteo.qmr &
 vmpk &
-
 sleep 5 &&
 
 aconnect -l &&
@@ -31,21 +31,31 @@ alsa_through=14
 kb=$(aconnect -l | grep "Origin49" | cut -f 1 -d ":" | cut -f 2 -d " ")
 vkb_o=$(aconnect -l | grep "VMPK Output" | cut -f 1 -d ":" | cut -f 2 -d " ")
 vkb_i=$(aconnect -l | grep "VMPK Input" | cut -f 1 -d ":" | cut -f 2 -d " ")
+router=$(aconnect -l | grep "qmidiroute" | cut -f 1 -d ":" | cut -f 2 -d " ")
 synth=$(aconnect -l | grep "FLUID" | cut -f 1 -d ":" | cut -f 2 -d " ")
 
-if [ "$vkb_o" ] && [ "$synth" ]
+if [ "$vkb_o" ]
 then
 	echo "Conectando $vkb_o:$alsa_through"
 	aconnect $vkb_o $alsa_through
-	echo "Conectando $alsa_through:$synth"
+	echo "Lizteilor!"
+fi
+
+if [ "$kb" ] && [ "$router" ]
+then
+	echo "Conectando $kb:$router"
+	aconnect $kb $router
+	echo "Lizteilor!"
+fi
+if [ "$router" ]
+then
+	echo "Conectando $router:$alsa_through"
+	aconnect $router\:1 $alsa_through
+	echo "Lizteilor!"
+fi
+
+if [ "$synth" ]
+then
 	aconnect $alsa_through $synth
 	echo "Lizteilor!"
 fi
-if [ "$kb" ] && [ "$vkb_i" ]
-then
-	echo "Conectando $kb:$vkb_i"
-	aconnect $kb $vkb_i
-	echo "Lizteilor!"
-fi
-
-
---
+++ stop.sh
@@ -0,0 +1,6 @@
+#/usr/bin/env bash
+killall vmpk
+killall fluidsynth
+killall qmidiroute
+killall jackd
+
